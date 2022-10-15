174b64dc1d4864c958238bab439cbd0143680dae
Deloz09 <84214640+TheDeloz-v2@users.noreply.github.com>
2022-10-15 17:16:26 -0600

    Update 2.2
    Arreglo de errores en base a solucionario.

--- DataPersistence/android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
+++ DataPersistence/android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.busschedule
 
 import android.os.Bundle
@@ -73,8 +58,6 @@ class StopScheduleFragment: Fragment() {
         recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         val busStopAdapter = BusStopAdapter({})
-        // by passing in the stop name, filtered results are returned,
-        // and tapping rows won't trigger navigation
         recyclerView.adapter = busStopAdapter
         lifecycle.coroutineScope.launch {
             viewModel.scheduleForStopName(stopName).collect() {
